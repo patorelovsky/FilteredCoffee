59c741c234e96b8aa5e00afebe6b8b051b00e364
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-19 21:41:36 +0100

    Add migration

---
+++ Data/Migrations/20240119203940_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilteredCoffee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brewing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CoffeeAmountGrams = table.Column<int>(type: "INTEGER", nullable: false),
+                    GrinderUsed = table.Column<string>(type: "TEXT", nullable: false),
+                    GrindLevelInClicks = table.Column<int>(type: "INTEGER", nullable: false),
+                    WaterTemperature = table.Column<int>(type: "INTEGER", nullable: false),
+                    BrewingTime = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brewing", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Coffee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CoffeeName = table.Column<string>(type: "TEXT", nullable: false),
+                    RoastLevel = table.Column<string>(type: "TEXT", nullable: true),
+                    Origin = table.Column<string>(type: "TEXT", nullable: true),
+                    FlavorProfile = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: true),
+                    StockQuantity = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coffee", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brewing");
+
+            migrationBuilder.DropTable(
+                name: "Coffee");
+        }
+    }
+}
