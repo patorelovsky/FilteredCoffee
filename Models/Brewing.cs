f0b659e6d147f8f24816fb261e1ca98eeea2f7fc
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-19 19:06:17 +0100

    create Brewing model

---
+++ Models/Brewing.cs
@@ -0,0 +1,17 @@
+﻿namespace FilteredCoffee;
+
+public class Brewing
+{
+    // Unique identifier for each coffee brewing record
+    public int Id { get; set; }
+    // The quantity of coffee used for brewing, measured in grams
+    public int CoffeeAmountGrams { get; set; }
+    // The type or brand of the coffee grinder used
+    public string GrinderUsed { get; set; }
+    // The grind level of the coffee beans, measured in clicks on the grinder
+    public int GrindLevelInClicks { get; set; }
+    // The temperature of the water used in brewing
+    public int WaterTemperature { get; set; }
+    // Recommended brewing time in minutes
+    public int BrewingTime { get; set; }
+}
