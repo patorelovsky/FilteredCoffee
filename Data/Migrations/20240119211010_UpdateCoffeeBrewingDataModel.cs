f096c565a85349e95c1df78e23bfdcdadac51f83
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-19 22:12:08 +0100

    Add update coffee brewing data model migration

---
+++ Data/Migrations/20240119211010_UpdateCoffeeBrewingDataModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilteredCoffee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCoffeeBrewingDataModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CoffeeId",
+                table: "Brewing",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoffeeId",
+                table: "Brewing");
+        }
+    }
+}
