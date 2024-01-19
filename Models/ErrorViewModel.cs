7d4a57629a38149f640c5a67ff8a714eb9c0190f
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-19 18:31:01 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FilteredCoffee.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
